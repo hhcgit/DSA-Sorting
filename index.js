bbdffc858333d694eb9f520aa1c5de3bd361a438
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-06-20 12:59:17 -0700

    added 6

--- index.js
+++ index.js
@@ -210,3 +210,79 @@ const list='89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 
 // return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
 // }
 // console.log(mSort(list))
+
+// const LinkedList = require('./LinkedList');
+// const sortList = new LinkedList;
+
+// nums.forEach(num =>{
+//     sortList.insertLast(num)
+// })
+
+// function mSortLinkedList(list){
+//     while(list.value!==null){
+
+//     }
+// }
+
+// function insertionSort(array) {
+//     var length = array.length;
+    
+//     for(var i = 1; i < length; i++) {
+//       var temp = array[i];
+//       for(var j = i - 1; j >= 0 && array[j] > temp; j--) {
+//         array[j+1] = array[j];
+//       }
+//       array[j+1] = temp;
+//     }
+    
+//     return array;
+//   }
+  
+//   // Implement bucket sort
+//   function bucketSort(array, bucketSize) {
+//     if (array.length === 0) {
+//       return array;
+//     }
+  
+//     // Declaring vars
+//     var i,
+//         minValue = array[0],
+//         maxValue = array[0],
+//         bucketSize = bucketSize || 5;
+    
+//     // Setting min and max values
+//     array.forEach(function (currentVal) {
+//         if (currentVal < minValue) {
+//             minValue = currentVal;
+//         } else if (currentVal > maxValue) {
+//             maxValue = currentVal;
+//         }
+//     })
+  
+//     // Initializing buckets
+//     var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+//     var allBuckets = new Array(bucketCount);
+    
+//     for (i = 0; i < allBuckets.length; i++) {
+//       allBuckets[i] = [];
+//     }
+    
+//     // Pushing values to buckets
+//     array.forEach(function (currentVal) {
+//         allBuckets[Math.floor((currentVal - minValue) / bucketSize)].push(currentVal);
+//     });
+  
+//     // Sorting buckets
+//     array.length = 0;
+    
+//     allBuckets.forEach(function(bucket) {
+//         insertionSort(bucket);
+//         bucket.forEach(function (element) {
+//             array.push(element)
+//         });
+//     });
+  
+//     return array;
+//   }
+
+//sort in place
